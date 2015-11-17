52f878c8a837d0661780da8f56c6cce1e154dddc
PaintX <remy.arnaudin@wanadoo.fr>
2015-11-17 20:07:34 +0100

    - debut codage printf.c

---
+++ LibC/stdio/c_stdio.h
@@ -0,0 +1,8 @@
+#ifndef C_STDIO_H_INCLUDED
+#define C_STDIO_H_INCLUDED
+
+
+int c_printf(const char *fmt , ...);
+
+
+#endif // C_STDIO_H_INCLUDED
--- main.c
+++ main.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "c_stdio.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    c_printf("Hello world!\n");
     return 0;
 }
